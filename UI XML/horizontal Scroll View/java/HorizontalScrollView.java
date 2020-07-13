98f8838909082f5dcf04dc0af0dd79e230626d7c
iamvatsal <vatsalkesarwani12@gmail.com>
2020-07-13 18:55:38 +0530

    IMPLEMENT: HorizontalScrollView

---
+++ UI XML/horizontal Scroll View/java/HorizontalScrollView.java
@@ -0,0 +1,16 @@
+package e.vatsalkesarwani.intentsndservices.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import e.vatsalkesarwani.intentsndservices.R;
+
+public class HorizontslScrollView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontsl_scroll_view);
+    }
+}
