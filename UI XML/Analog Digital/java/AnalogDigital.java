b14998a9277c728ab398bb85044b8a2ae104f73d
iamvatsal <vatsalkesarwani12@gmail.com>
2020-07-13 18:30:26 +0530

    IMPLEMENT: Analog and Digital

---
+++ UI XML/Analog Digital/java/AnalogDigital.java
@@ -0,0 +1,16 @@
+package e.vatsalkesarwani.intentsndservices.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import e.vatsalkesarwani.intentsndservices.R;
+
+public class AnalogDigital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analog_digital);
+    }
+}
