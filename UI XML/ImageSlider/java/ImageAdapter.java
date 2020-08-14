f12044c678316760e3d3b32931b4f3235094f1b6
jyotijauhari222@gmail.com <jyotijauhari222@gmail.com>
2020-08-14 16:31:53 +0530

    Image Slider

---
+++ UI XML/ImageSlider/java/ImageAdapter.java
@@ -0,0 +1,45 @@
+package com.dj.imageslider;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class ImageAdapter extends PagerAdapter {
+
+    Context mContext;
+
+    ImageAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    private int[] sliderImageId = {R.drawable.g1, R.drawable.g2,R.drawable.g3, R.drawable.g4,R.drawable.g5, R.drawable.g6,R.drawable.g7, R.drawable.g8 } ;
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(sliderImageId[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+
+    @Override
+    public int getCount() {
+        return sliderImageId.length;
+    }
+}
---
+++ UI XML/ImageSlider/java/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dj.imageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPage);
+        ImageAdapter adapterView = new ImageAdapter(this);
+        mViewPager.setAdapter(adapterView);
+    }
+}
