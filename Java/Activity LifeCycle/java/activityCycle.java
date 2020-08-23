522256e7f7946862f22de33f24ebdc0ba8e938b2
iamvatsal <vatsalkesarwani12@gmail.com>
2020-08-24 01:58:35 +0530

    IMPLEMENT: Activity LifeCycle

---
+++ Java/Activity LifeCycle/java/activityCycle.java
@@ -0,0 +1,55 @@
+package e.vatsalkesarwani.intentsndservices.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import e.vatsalkesarwani.intentsndservices.R;
+
+public class ActivityCycle extends AppCompatActivity {
+    private static final String TAG = "ActivityCycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cycle);
+
+        Log.i(TAG , "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG , "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG , "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG , "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG , "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG , "onRestart");
+    }
+}
