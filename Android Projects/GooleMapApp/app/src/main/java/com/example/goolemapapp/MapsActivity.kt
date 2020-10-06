262e699b8377c16b7916a769e16845427753e915
I514375 <abhishek.rustagi@sap.com>
2020-10-06 18:16:19 +0530

    google map implement

---
+++ Android Projects/GooleMapApp/app/src/main/java/com/example/goolemapapp/MapsActivity.kt
@@ -0,0 +1,47 @@
+package com.example.goolemapapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val bangalore = LatLng(12.9716, 77.5946)
+        mMap.addMarker(MarkerOptions().position(bangalore).title("Marker in Bangalore"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bangalore))
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
