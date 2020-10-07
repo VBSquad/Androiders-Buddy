10521d23745acaab9b19eb8d025e8ff2782d949a
I514375 <abhishek.rustagi@sap.com>
2020-10-07 10:44:53 +0530

    delete files

---
+++ Android Projects/GooleMapApp/app/src/main/java/com/example/goolemapapp/HomeScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.goolemapapp;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this,MapsActivity.class);
+            }
+        });
+    }
+}
